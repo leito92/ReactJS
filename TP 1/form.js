3639965ab36185489ac5512253a371734caabfb5
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-05-01 17:19:16 -0300

    remove onchange for javascript only

--- TP 1/form.js
+++ TP 1/form.js
@@ -1,24 +1,18 @@
+const type = document.getElementById("type")
+const value = document.getElementById("value")
+const text = "El valor estimado para el tipo de seguro seleccionado es de: $"
+
+type.addEventListener("change", valorTipoSeguro)
 function valorTipoSeguro() {
-    switch(document.getElementById("tipo").value){
-        case "Basico":
-            document.getElementById("value").innerHTML = "$ 500"
+    switch(document.getElementById("type").value){
+        case "Básico":
+            value.innerText = text + "500"
             break;
         case "Intermedio":
-            document.getElementById("value").innerHTML = "$ 1.000"
+            value.innerText = text + "1.000"
             break;
         case "Premium":
-            document.getElementById("value").innerHTML = "$ 1.500"
+            value.innerText = text + "1.500"
             break;
     }
-}
-
-function send() {
-    var name = document.getElementById("name").value
-    var lastname = document.getElementById("lastname").value
-    var dni = document.getElementById("dni").value
-    var email = document.getElementById("email").value
-    var phone = document.getElementById("phone").value
-    var tipoSeguro = document.getElementById("tipo").value
-    console.log(name, lastname, dni, email, phone, tipoSeguro)
-    alert("Formulario enviado!")
 }
