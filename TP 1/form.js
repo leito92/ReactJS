355327411e9dcdd0e31120da3fa605a4d7c0c24e
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-05-04 14:15:09 -0300

    fix

--- TP 1/form.js
+++ TP 1/form.js
@@ -1,10 +1,11 @@
 const type = document.getElementById("type")
 const value = document.getElementById("value")
 const text = "El valor estimado para el tipo de seguro seleccionado es de: $"
+const button = document.getElementById("send")
 
 type.addEventListener("change", valorTipoSeguro)
 function valorTipoSeguro() {
-    switch(document.getElementById("type").value){
+    switch(type.value){
         case "Básico":
             value.innerText = text + "500"
             break;
@@ -14,5 +15,18 @@ function valorTipoSeguro() {
         case "Premium":
             value.innerText = text + "1.500"
             break;
+    }
+}
+
+button.addEventListener("click", sendForm)
+function sendForm() {
+    let name = document.getElementById("name").value
+    let lastname = document.getElementById("lastname").value
+    let dni = document.getElementById("dni").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    console.log(name, lastname, dni, email, phone, type)
+    if (!!name && !!lastname && !!dni && !!email && !!phone && !!type.value) {
+        alert("Formulario enviado!")
     }
 }
