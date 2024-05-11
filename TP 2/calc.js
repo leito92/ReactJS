0b2010ab5d9dc91b8f0865e5532de1dbbfed180f
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-05-11 15:42:10 -0300

    fix

--- TP 2/calc.js
+++ TP 2/calc.js
@@ -1,32 +1,32 @@
-const n1 = document.getElementById("number1")
-const n2 = document.getElementById("number2")
-const resultado = document.getElementById("resultado")
+const n1 = document.getElementById("number1");
+const n2 = document.getElementById("number2");
+const resultado = document.getElementById("resultado");
 
-const suma = () => {
+const suma = (num1, num2) => num1 + num2;
+const resta = (num1, num2) => num1 - num2;
+const multiplicacion = (num1, num2) => num1 * num2;
+const division = (num1, num2) => num1 / num2;
+
+function calculator(num1, num2, op) {
+    return op(num1, num2);
+};
+document.getElementById("suma").addEventListener("click", () => {
     if (!!n1.value && !!n2.value) {
-        resultado.innerText = "El resultado es: " + (parseFloat(n1.value) + parseFloat(n2.value))
+        resultado.innerText = "El resultado es: " + calculator(parseFloat(n1.value), parseFloat(n2.value), suma);
     }
-}
-document.getElementById("suma").addEventListener("click", suma)
-
-const resta = () => {
+});
+document.getElementById("resta").addEventListener("click", () => {
     if (!!n1.value && !!n2.value) {
-        resultado.innerText = "El resultado es: " + (parseFloat(n1.value) - parseFloat(n2.value))
+        resultado.innerText = "El resultado es: " + calculator(parseFloat(n1.value), parseFloat(n2.value), resta);
     }
-}
-document.getElementById("resta").addEventListener("click", resta)
-
-
-const multiplicacion = () => {
+});
+document.getElementById("multiplicación").addEventListener("click", () => {
     if (!!n1.value && !!n2.value) {
-        resultado.innerText = "El resultado es: " + (parseFloat(n1.value) * parseFloat(n2.value))
+        resultado.innerText = "El resultado es: " + calculator(parseFloat(n1.value), parseFloat(n2.value), multiplicacion);
     }
-}
-document.getElementById("multiplicación").addEventListener("click", multiplicacion)
-
-const division = () => {
+});
+document.getElementById("división").addEventListener("click", () => {
     if (!!n1.value && !!n2.value && n2.value != 0) {
-        resultado.innerText = "El resultado es: " + (parseFloat(n1.value) / parseFloat(n2.value))
+        resultado.innerText = "El resultado es: " + calculator(parseFloat(n1.value), parseFloat(n2.value), division);
     }
-}
-document.getElementById("división").addEventListener("click", division)
+});
