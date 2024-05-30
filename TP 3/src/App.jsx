c15e6a299ff905295e2b7380b983248c72e03bf8
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-05-30 18:47:26 -0300

    using useState & useEffect

--- TP 3/src/App.jsx
+++ TP 3/src/App.jsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
 import List from "./components/List";
-import data from "./data/products.json";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => { 
+    fetch("https://6658ec85de346625136ae3f0.mockapi.io/products")
+      .then((res) => res.json())
+      .then((data) => setData(data)); 
+  }, []);
+
   return (
     <List products={data}/>
   );
