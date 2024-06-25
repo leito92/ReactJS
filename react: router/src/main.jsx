fdedc9ccaa5a9c7514cdd9b028943fd3f9cac483
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-06-25 15:02:29 -0300

    fix

--- react: router/src/main.jsx
+++ react: router/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Signup from './components/Signup/index.jsx'
+import Signup from './pages/Signup/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
